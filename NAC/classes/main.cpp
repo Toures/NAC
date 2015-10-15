6b7dc00786d6f553f33dbe42a39fee75f758d798
Toures <Mike.Hukiewitz@gmx.de>
2015-10-15 13:26:37 +0200

    + Aufgabe 3.2

--- NAC/classes/main.cpp
+++ NAC/classes/main.cpp
@@ -12,5 +12,14 @@ int main() {
 	c.addiere(a);
 	b.addiere(c);
 	std::cout << "Betrag c: " << c.betrag() << std::endl;
-	std::cout << "Betrag b: " << b.betrag() << std::endl;	
+	std::cout << "Betrag b: " << b.betrag() << std::endl;
+
+	Vektor2D *w = new Vektor2D();
+	Vektor2D *z = new Vektor2D(-3,1);
+	a.addiere(*z);
+	std::cout << "Betrag a: "; a.ausgabe(); std::cout << std::endl;
+	w->addiere(*z);
+	std::cout << "Betrag w: "; w->ausgabe(); std::cout << std::endl;
+	w->addiere(b);
+	std::cout << "Betrag w: "; w->ausgabe(); std::cout << std::endl;
 }
